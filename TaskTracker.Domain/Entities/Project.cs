698d4476ed2e1d19fcee2d786ef0b5861390cd5d
Aleksa <aleksalevnajic@gmail.com>
2023-12-14 16:37:36 +0100

    Added model validation

--- TaskTracker.Domain/Entities/Project.cs
+++ TaskTracker.Domain/Entities/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,12 @@ namespace TaskTracker.DataAccess.Entities
 {
     public class Project : BaseEntity
     {
+        [Required(ErrorMessage = "Project Name is required.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Start date is requierd.")]
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
+        [Range(1, 100, ErrorMessage = "Priority must be greater than 0.")]
         public int Priority { get; set; }
         public ProjectStatus Status { get; set; }
 
--- TaskTracker.Domain/Entities/Task.cs
+++ TaskTracker.Domain/Entities/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
@@ -7,10 +8,13 @@ namespace TaskTracker.DataAccess.Entities
 {
     public class Task : BaseEntity
     {
+        [Required(ErrorMessage = "Task Name is requird")]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Range(1, 100, ErrorMessage = "Priority must be greater than 0.")]
         public int  Priority { get; set; }
         public TaskStatus Status { get; set; }
+        [Required(ErrorMessage = "ProjectId is requird")]
         public int ProjectId { get; set; }
 
         public Project Project { get; set; }
